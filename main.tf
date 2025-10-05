8028a365cf3fdfd67819aa5b1af6956be6b9b6b0
Shyam Bahadur <shyamshaan091223@gmail.com>
2025-10-05 15:30:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-# resource "azurerm_resource_group" "rg" {
-#   name     = "rg-virat"
-#   location = "West US"
-# }
+resource "azurerm_resource_group" "rg" {
+   name     = "rg-virat"
+   location = "West US"
+ }
 
-# resource "azurerm_resource_group" "rg1" {
-#   name     = "rg-gill"
-#   location = "West US"
-# }
+ resource "azurerm_resource_group" "rg1" {
+   name     = "rg-gill"
+   location = "West US"
+
+ }
