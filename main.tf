0ded98f5fa25a469001a11190c6d19de29dbafe1
Shyam Bahadur <shyamshaan091223@gmail.com>
2025-10-05 15:29:49 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-virat"
-  location = "West US"
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = "rg-virat"
+#   location = "West US"
+# }
 
 # resource "azurerm_resource_group" "rg1" {
 #   name     = "rg-gill"
